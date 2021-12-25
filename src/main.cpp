c67bfbd7ca1e81653c0f8655aa13feeb7a5cdd8a
uecken <the.full.of.moon@gmail.com>
2021-12-25 16:42:43 +0900

    add bad/good judge

--- src/main.cpp
+++ src/main.cpp
@@ -3,12 +3,15 @@
 BleKeyboard bleKeyboard;
 
 #define SAMPLE_SIZE 4000
-#define SAMPLE_PERIOD 30
+#define SAMPLE_PERIOD 20
 float ax[SAMPLE_SIZE], ay[SAMPLE_SIZE], az[SAMPLE_SIZE];
-float gx[SAMPLE_SIZE], gy[SAMPLE_SIZE], gz[SAMPLE_SIZE];
+float ax_prev =0,ay_prev=0,az_prev=0;
+//float gx[SAMPLE_SIZE], gy[SAMPLE_SIZE], gz[SAMPLE_SIZE];
+
+
 long i = 0;
 
-boolean ACC_DEBUG = true;
+boolean ACC_DEBUG = false;
 boolean JUDGE_DEBUG = true;
 
 void setup() {
@@ -21,28 +24,36 @@ void setup() {
   bleKeyboard.begin();
 }
 
-boolean eventDetection(String event ="acc_abs_event"){
+boolean eventDetector(String sensor ="accz"){
+  boolean eventDetected = false;
   M5.IMU.getAccelData(&ax[i],&ay[i],&az[i]);
   if(ACC_DEBUG)Serial.printf("%4.2f %4.2f %4.2f \n",ax[i],ay[i],az[i]);
-  if(event = "acc_abs_event"){
+  if(sensor = "accz"){
+    float az_th = 3.5;
     if(i != 0){
-      if(abs(az[i] - az[i-1]) > 3){
+      if((az[i] - az_prev) > az_th){
+        if(ACC_DEBUG)Serial.printf("ACC-Z Event Detected. ACCz-diff is %4.2f \n",az[i] - az[i-1]);
+        eventDetected =  true;
+      }
+    }
+    /*
+    else if(i == SAMPLE_SIZE){
+      if((az[0] - az[SAMPLE_SIZE])>az_th){
         if(ACC_DEBUG)Serial.printf("ACC Event Detected. ACCz-diff is %4.2f \n",az[i] - az[i-1]);
         return true;
       }
-    }else if(i == SAMPLE_SIZE){
-      if(abs(az[0] - az[SAMPLE_SIZE])>5000){
-        if(JUDGE_DEBUG)Serial.printf("%4,2f",az[i] - az[i-1]);
-        return true;
-      }
     }
-  }else if(event == "grounding_event"){
-  }else if(event == "pressure_event" ){   
+    */
+  }else if(sensor == "grounding"){
+  }else if(sensor == "pressure_sensor" ){
   }
-  return false;
+
+  ax_prev = ax[i]; ay_prev = ay[i]; az_prev = az[i];
+  return eventDetected;
 }
 
 void eventAction(String actionType="print",String key="e"){
+  Serial.printf("actionType:%s key:%s \n",actionType,key);
   if(actionType = "print"){
     bleKeyboard.print(key);  
   }else if(actionType = "write"){
@@ -68,9 +79,18 @@ void loop() {
     previousTime = millis();
     if(bleKeyboard.isConnected()){  
       delay(20);
-      if(eventDetection("acc_abs_event")){
-        eventAction("write");
+      if(eventDetector("accz_sensor")){
+        eventAction("print");
+        //複数回eventが実行されてしまう。
+        //１．接地検出前後ではなく、接地時にevent実行される工夫を入れ、
+        //２．接地前後では実行されないように、また連続実行されない処理とすること
       }
+      if(eventDetector("pressure_sensor")){
+        //感圧センサイベント処理を入れること
+        //ACCZセンサー同様に複数回検知させないこと。
+        eventAction("print");
+      }
+
     
       if(M5.BtnA.isPressed()){
         Serial.println("BtnA is pressed");
@@ -82,10 +102,12 @@ void loop() {
         eventClear();
       }
 
+
+
       //====bleKeyboard.print is best for Songle=====
       if(M5.BtnB.wasPressed()){
         Serial.println("BtnB is pressed");
-        eventAction("write","e");
+        eventAction("print","e");
         BtnBisPressed = true;
       }
       else if(M5.BtnB.wasReleased()){
